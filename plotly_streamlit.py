9bf7538cbcdf6aef38bedb72b33a6ee5ad0d8670
Madwaraj <38559186+Madwaraj@users.noreply.github.com>
2021-03-29 00:46:33 +0200

    Update plotly_streamlit.py

--- plotly_streamlit.py
+++ plotly_streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
 
 # st.cache caches the function commands so that it is not reloaded every time the script is updated.
 
